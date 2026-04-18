ae92cccc02328f2f47766cad10fb5f08e0d42bc4
alim344 <milabudimirovic2@gmail.com>
2026-04-18 11:36:44 +0200

    Init

---
+++ sotexhakaton/src/main/java/com/example/sotexhakaton/SotexhakatonApplication.java
@@ -0,0 +1,13 @@
+package com.example.sotexhakaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotexhakatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotexhakatonApplication.class, args);
+	}
+
+}
---
+++ sotexhakaton/src/test/java/com/example/sotexhakaton/SotexhakatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sotexhakaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotexhakatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
